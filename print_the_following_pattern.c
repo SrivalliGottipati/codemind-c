e3a582cef74d505ea2f2a2855930c0129b057b06
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-01-15 21:14:16 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1;i<=n;i++)     {         for(int j=1;j<=n;j++)         {             printf("%c ",i+64);             }             printf(" ");             } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,21 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n; 
-    scanf("%d",&n);
+    int n;
+    scanf("%d",&n);    
     for(int i=1;i<=n;i++)
     {
-        for(int j=1;j<=n;j++) 
+        for(int j=1;j<=n;j++)
         {
-            if(i==j||j==n+1-i)
-            {
-                printf("x");    
-                }
-                else    
-                {
-                    printf("0");    
-                    }
-                    }
-                    printf("
+            printf("%c ",i+64);     
+            }
+            printf("
 ");
-                    }}
+            }
+}
