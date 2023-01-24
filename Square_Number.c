c2f00841d4b497d32cfa5f8e84bdd49e8aa95964
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-01-24 19:44:01 +0530

    #include<stdio.h> #include<math.h> int main() {     int n;     scanf("%d",&n);     int a=sqrt(n);     for(int i=0;i<=n;i++)     {         for(int j=1;j<=n;j++)         {             if(i*i+j*j==n)             {                 printf("True");                 return 0;                 }                 }                 }                 printf("False");
    }

---
+++ Square_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a=sqrt(n);
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(i*i+j*j==n)
+            {
+                printf("True");
+                return 0;
+                }
+                }
+                }
+                printf("False");
+    
+}
