32eb6faef7f52c4fddb02a8bfe966eb2b314e8f2
SrivalliGottipati <121477373+SrivalliGottipati@users.noreply.github.com>
2023-02-03 22:13:53 +0530

    #include<stdio.h> int main() {     char x;     scanf("%c",&x);     if(x=='A')     {         printf("Vowel");     }     else if(x=='e')     {         printf("Vowel");     }     else if(x=='i')     {         printf("Vowel");     }     else if(x=='o')     {         printf("Vowel");     }     else if(x=='u')     {         printf("Vowel");
        }else{printf("Consonant");}}

---
+++ Program_to_check_Vowel_or_Consonant.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    char x;
+    scanf("%c",&x);
+    if(x=='A')
+    {
+        printf("Vowel");
+    }
+    else if(x=='e')
+    {
+        printf("Vowel");
+    }
+    else if(x=='i')
+    {
+        printf("Vowel");
+    }
+    else if(x=='o')
+    {
+        printf("Vowel");
+    }
+    else if(x=='u')
+    {
+        printf("Vowel");
+        
+    }else{printf("Consonant");}}
